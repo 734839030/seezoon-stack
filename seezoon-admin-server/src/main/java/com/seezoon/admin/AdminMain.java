27c5061c11e7788cde9af6e3771b1ef040dd728b
dfenghuang <dfenghuang@tencent.com>
2020-11-11 22:08:13 +0800

    async

--- seezoon-admin-server/src/main/java/com/seezoon/admin/AdminMain.java
+++ seezoon-admin-server/src/main/java/com/seezoon/admin/AdminMain.java
@@ -2,7 +2,6 @@ package com.seezoon.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * 程序入口
@@ -10,7 +9,6 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
  * @author hdf
  */
 @SpringBootApplication
-@EnableTransactionManagement(proxyTargetClass = true)
 public class AdminMain {
 
     public static void main(String[] args) {
