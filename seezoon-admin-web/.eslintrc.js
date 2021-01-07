7dfb82d32fc3f3f7872e1669e63781ffbe6540c5
hdf <hdf@qq.com>
2021-01-08 02:02:47 +0800

    fix:remove web

--- seezoon-admin-web/.eslintrc.js
+++ seezoon-admin-web/.eslintrc.js
@@ -1,27 +1,25 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  'extends': [
-    'plugin:vue/strongly-recommended',
-    '@vue/standard'
-  ],
-  rules: {
-    
-  },
-  parserOptions: {
-    parser: 'babel-eslint'
-},
-  overrides: [
-  {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)'
-      ],
-      env: {
-        jest: true
-      }
-    }
-  ]
+    root: true,
+    env: {
+        node: true
+    },
+    'extends': [
+        'plugin:vue/strongly-recommended',
+        '@vue/standard'
+    ],
+    rules: {},
+    parserOptions: {
+        parser: 'babel-eslint'
+    },
+    overrides: [
+        {
+            files: [
+                '**/__tests__/*.{j,t}s?(x)',
+                '**/tests/unit/**/*.spec.{j,t}s?(x)'
+            ],
+            env: {
+                jest: true
+            }
+        }
+    ]
 }
