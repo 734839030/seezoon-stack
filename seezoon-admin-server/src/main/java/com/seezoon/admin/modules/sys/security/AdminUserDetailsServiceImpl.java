30e97f7a368718a0e111762ce7bd06f5baaced3e
hdf <hdf@qq.com>
2021-03-11 02:53:12 +0800

    fix: user permissions

--- seezoon-admin-server/src/main/java/com/seezoon/admin/modules/sys/security/AdminUserDetailsServiceImpl.java
+++ seezoon-admin-server/src/main/java/com/seezoon/admin/modules/sys/security/AdminUserDetailsServiceImpl.java
@@ -1,8 +1,12 @@
 package com.seezoon.admin.modules.sys.security;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -11,6 +15,8 @@ import com.seezoon.admin.framework.file.FileService;
 import com.seezoon.admin.modules.sys.dto.UserInfo;
 import com.seezoon.admin.modules.sys.service.SysUserService;
 import com.seezoon.dao.framework.constants.EntityStatus;
+import com.seezoon.dao.modules.sys.entity.SysMenu;
+import com.seezoon.dao.modules.sys.entity.SysRole;
 import com.seezoon.dao.modules.sys.entity.SysUser;
 
 /**
@@ -46,6 +52,26 @@ public class AdminUserDetailsServiceImpl implements UserDetailsService {
         userInfo.setPhoto(fileService.getUrl(user.getPhoto()));
         // 角色及权限信息登录成功后放入
         AdminUserDetails adminUserDetails = new AdminUserDetails(userInfo, username, user.getPassword(), false);
+        adminUserDetails.setAuthorities(getAuthorities(user.getId()));
         return adminUserDetails;
+    }
+
+    /**
+     * 登录成功后再放入权限,也可以在{@link AdminUserDetailsServiceImpl}中放，不过如果恶意刷库会导致内存占用和DB IO
+     *
+     * @return
+     */
+    private List<GrantedAuthority> getAuthorities(Integer userId) {
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        // 角色处理
+        List<SysRole> userRoles = userService.findRolesByUserId(userId);
+        userRoles.stream().filter(v -> StringUtils.isNotBlank(v.getCode())).forEach(v -> {
+            authorities.add(new UserGrantedAuthority(v.getCode(), true));
+        });
+        List<SysMenu> userMenus = userService.findMenusByUserId(userId);
+        userMenus.stream().filter(v -> StringUtils.isNotBlank(v.getPermission())).forEach(v -> {
+            authorities.add(new UserGrantedAuthority(v.getPermission()));
+        });
+        return authorities;
     }
 }
