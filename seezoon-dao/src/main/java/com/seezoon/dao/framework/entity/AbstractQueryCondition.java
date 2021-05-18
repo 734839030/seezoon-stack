eb10d89342d02e10f8339ee130e5ec15048fa07f
hdf <hdf@qq.com>
2021-05-19 01:18:48 +0800

    fix(query): empty query condition

--- seezoon-dao/src/main/java/com/seezoon/dao/framework/entity/AbstractQueryCondition.java
+++ seezoon-dao/src/main/java/com/seezoon/dao/framework/entity/AbstractQueryCondition.java
@@ -7,7 +7,10 @@ package com.seezoon.dao.framework.entity;
  */
 public abstract class AbstractQueryCondition {
 
-    public static final EmptyQueryCondition EMPTY = new EmptyQueryCondition();
+    /**
+     * 重载的方法当传null 语法错误问题
+     */
+    public static final EmptyQueryCondition EMPTY = null;
 
     private static final class EmptyQueryCondition extends AbstractQueryCondition {
 
