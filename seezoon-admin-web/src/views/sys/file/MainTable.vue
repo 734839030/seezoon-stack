d3c887f61d6c42f1d97e53c380fc7b2849f65ffe
bo_huang <bo_huang@shssyw.com>
2021-08-03 23:43:14 +0800

    perf(文件管理):增加批量删除功能。

--- seezoon-admin-web/src/views/sys/file/MainTable.vue
+++ seezoon-admin-web/src/views/sys/file/MainTable.vue
@@ -32,9 +32,16 @@
           name="file"
         >
           <a-button v-auth="'sys:file:upload'" :loading="uploadBtnLoading" type="default">
-            <cloud-upload-outlined />
+            <cloud-upload-outlined />上传
           </a-button>
         </a-upload>
+        <a-button
+          v-auth="'sys:file:delete'"
+          :disabled="selectedRowKeys.length == 0"
+          type="default"
+          @click="batchDelete(selectedRowKeys)"
+          >删除
+        </a-button>
       </a-space>
     </a-form-item>
   </a-form>
@@ -45,6 +52,7 @@
     :pagination="pagination"
     :row-key="(record) => record.id"
     bordered
+    :row-selection="{ selectedRowKeys: selectedRowKeys, onChange: onSelectChange }"
     class="mt-4"
     size="small"
     @change="handleTableChange"
@@ -82,6 +90,7 @@
           createDateRange: [moment().day(-7).format('YYYY-MM-DD'), moment().format('YYYY-MM-DD')],
         },
         url: '/sys/file/query',
+        selectedRowKeys: [],
         columns: [
           {
             title: '文件名',
@@ -114,6 +123,9 @@
       this.handleQuery();
     },
     methods: {
+      onSelectChange(selectedRowKeys) {
+        this.selectedRowKeys = selectedRowKeys;
+      },
       preview(url) {
         openWindow(url);
       },
@@ -132,6 +144,17 @@
             this.handleQuery();
           })
           .finally(() => (this.uploadBtnLoading = false));
+      },
+      batchDelete(selectedRowKeys) {
+        defHttp.postForm('/sys/file/delete_batch', { ids: selectedRowKeys.join(',') })
+          .then(() => {
+            this.$message.success('删除成功');
+            this.handleQuery();
+          })
+          .catch((error) => {
+            console.info(error);
+            this.$message.error(error);
+          });
       },
     },
   };
