3651eb98eaf6bb4fa9b46cdee8ad5ea726c6021b
dfenghuang <dfenghuang@tencent.com>
2021-08-22 09:00:14 +0800

    feat(minio): add minio support

--- seezoon-framework/src/main/java/com/seezoon/framework/component/file/handler/MinioHandler.java
+++ seezoon-framework/src/main/java/com/seezoon/framework/component/file/handler/MinioHandler.java
@@ -1,17 +1,21 @@
 package com.seezoon.framework.component.file.handler;
 
-import com.seezoon.framework.properties.SeezoonProperties;
-import io.minio.*;
-import io.minio.errors.*;
+import java.io.IOException;
+import java.io.InputStream;
+
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.util.Assert;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
+import com.seezoon.framework.properties.SeezoonProperties;
+
+import io.minio.BucketExistsArgs;
+import io.minio.GetObjectArgs;
+import io.minio.MakeBucketArgs;
+import io.minio.MinioClient;
+import io.minio.PutObjectArgs;
+import io.minio.RemoveObjectArgs;
 
 /**
  * @author bunco
@@ -23,35 +27,16 @@ public class MinioHandler implements FileHandler {
 
     public MinioHandler(SeezoonProperties.FileProperties fileProperties) {
         this.fileProperties = fileProperties;
-        this.minioClient = MinioClient.builder()
-                .endpoint(fileProperties.getMinio().getEndpoint())
-                .credentials(fileProperties.getMinio().getAccessKey(),
-                        fileProperties.getMinio().getSecretKey())
-                .build();
+        this.minioClient = MinioClient.builder().endpoint(fileProperties.getMinio().getEndpoint())
+            .credentials(fileProperties.getMinio().getAccessKey(), fileProperties.getMinio().getSecretKey()).build();
         try {
-            if (!this.minioClient.bucketExists(
-                    BucketExistsArgs.builder().bucket(fileProperties.getMinio().getBucketName()).build())) {
-                this.minioClient.makeBucket(
-                        MakeBucketArgs.builder().bucket(fileProperties.getMinio().getBucketName()).build());
+            if (!this.minioClient
+                .bucketExists(BucketExistsArgs.builder().bucket(fileProperties.getMinio().getBucketName()).build())) {
+                this.minioClient
+                    .makeBucket(MakeBucketArgs.builder().bucket(fileProperties.getMinio().getBucketName()).build());
             }
-        } catch (ErrorResponseException e) {
-            logger.error("", e);
-        } catch (InsufficientDataException e) {
-            logger.error("", e);
-        } catch (InternalException e) {
-            logger.error("", e);
-        } catch (InvalidKeyException e) {
-            logger.error("", e);
-        } catch (InvalidResponseException e) {
-            logger.error("", e);
-        } catch (IOException e) {
-            logger.error("", e);
-        } catch (NoSuchAlgorithmException e) {
-            logger.error("", e);
-        } catch (ServerException e) {
-            logger.error("", e);
-        } catch (XmlParserException e) {
-            logger.error("", e);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
     }
 
@@ -61,51 +46,18 @@ public class MinioHandler implements FileHandler {
             InputStream compressedInputStream =
                 imageCompress(in, fileProperties.getImageQuality(), fileProperties.getIamgeScale());
             try {
-                this.minioClient.putObject(PutObjectArgs.builder()
-                        .bucket(fileProperties.getMinio().getBucketName())
-                        .object(handleRelativePath(relativePath))
-                        .stream(compressedInputStream, -1, 10485760)
-                        .build());
-            } catch (ErrorResponseException e) {
-                logger.error("", e);
-            } catch (InsufficientDataException e) {
-                logger.error("", e);
-            } catch (InternalException e) {
-                logger.error("", e);
-            } catch (InvalidKeyException e) {
-                logger.error("", e);
-            } catch (InvalidResponseException e) {
-                logger.error("", e);
-            } catch (NoSuchAlgorithmException e) {
-                logger.error("", e);
-            } catch (ServerException e) {
-                logger.error("", e);
-            } catch (XmlParserException e) {
-                logger.error("", e);
+                this.minioClient.putObject(PutObjectArgs.builder().bucket(fileProperties.getMinio().getBucketName())
+                    .object(handleRelativePath(relativePath)).stream(compressedInputStream, -1, 10485760).build());
+            } catch (Exception e) {
+                // 正规项目自定义异常及错误码、文案
+                throw new RuntimeException(e);
             }
         } else {
             try {
-                this.minioClient.putObject(PutObjectArgs.builder()
-                        .bucket(fileProperties.getMinio().getBucketName())
-                        .object(handleRelativePath(relativePath))
-                        .stream(in, -1, 10485760)
-                        .build());
-            } catch (ErrorResponseException e) {
-                logger.error("", e);
-            } catch (InsufficientDataException e) {
-                logger.error("", e);
-            } catch (InternalException e) {
-                logger.error("", e);
-            } catch (InvalidKeyException e) {
-                logger.error("", e);
-            } catch (InvalidResponseException e) {
-                logger.error("", e);
-            } catch (NoSuchAlgorithmException e) {
-                logger.error("", e);
-            } catch (ServerException e) {
-                logger.error("", e);
-            } catch (XmlParserException e) {
-                logger.error("", e);
+                this.minioClient.putObject(PutObjectArgs.builder().bucket(fileProperties.getMinio().getBucketName())
+                    .object(handleRelativePath(relativePath)).stream(in, -1, 10485760).build());
+            } catch (Exception e) {
+                throw new RuntimeException(e);
             }
         }
         if (null != in) {
@@ -117,27 +69,11 @@ public class MinioHandler implements FileHandler {
     public InputStream download(String relativePath) throws IOException {
         InputStream stream = null;
         try {
-            stream = this.minioClient.getObject(GetObjectArgs.builder()
-                    .bucket(fileProperties.getMinio().getBucketName())
-                    .object(this.handleRelativePath(relativePath))
-                    .build()
-            );
-        } catch (ErrorResponseException e) {
-            logger.error("", e);
-        } catch (InsufficientDataException e) {
-            logger.error("", e);
-        } catch (InternalException e) {
-            logger.error("", e);
-        } catch (InvalidKeyException e) {
-            logger.error("", e);
-        } catch (InvalidResponseException e) {
-            logger.error("", e);
-        } catch (NoSuchAlgorithmException e) {
-            logger.error("", e);
-        } catch (ServerException e) {
-            logger.error("", e);
-        } catch (XmlParserException e) {
-            logger.error("", e);
+            stream =
+                this.minioClient.getObject(GetObjectArgs.builder().bucket(fileProperties.getMinio().getBucketName())
+                    .object(this.handleRelativePath(relativePath)).build());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
         return stream;
     }
@@ -145,26 +81,10 @@ public class MinioHandler implements FileHandler {
     @Override
     public void delete(String relativePath) throws IOException {
         try {
-            this.minioClient.removeObject(RemoveObjectArgs.builder()
-                    .bucket(fileProperties.getMinio().getBucketName())
-                    .object(this.handleRelativePath(relativePath))
-                    .build());
-        } catch (ErrorResponseException e) {
-            logger.error("", e);
-        } catch (InsufficientDataException e) {
-            logger.error("", e);
-        } catch (InternalException e) {
-            logger.error("", e);
-        } catch (InvalidKeyException e) {
-            logger.error("", e);
-        } catch (InvalidResponseException e) {
-            logger.error("", e);
-        } catch (NoSuchAlgorithmException e) {
-            logger.error("", e);
-        } catch (ServerException e) {
-            logger.error("", e);
-        } catch (XmlParserException e) {
-            logger.error("", e);
+            this.minioClient.removeObject(RemoveObjectArgs.builder().bucket(fileProperties.getMinio().getBucketName())
+                .object(this.handleRelativePath(relativePath)).build());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
     }
 
@@ -197,6 +117,6 @@ public class MinioHandler implements FileHandler {
     @Override
     public String getUrlPrefix() {
         return StringUtils.appendIfMissing(fileProperties.getUrlPrefix(), "/")
-                + StringUtils.appendIfMissing(fileProperties.getMinio().getBucketName(), "/");
+            + StringUtils.appendIfMissing(fileProperties.getMinio().getBucketName(), "/");
     }
 }
